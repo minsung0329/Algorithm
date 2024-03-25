e94574bed270e944a8786f7b649b9bd20f306666
minsung0329 <minsung021221@naver.com>
2024-03-25 20:44:27 +0900

    firstCommit

--- baekjoon/1001.c
+++ baekjoon/1001.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hello world!");
+    printf("%ld\n", sizeof(int));
+    printf("%ld\n", sizeof(double));
+    printf("%ld\n", sizeof(long));
+    printf("%ld\n", sizeof(short));
 }
